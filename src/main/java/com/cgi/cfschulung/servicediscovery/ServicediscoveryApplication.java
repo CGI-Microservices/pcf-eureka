6a838b7aced6d39e4c2e4d90544058c8cd3424f3
Daniel Beyer <danielbeyer@Daniels-MacBook-Pro.local>
2017-10-19 14:00:18 +0200

    new stuff

---
+++ src/main/java/com/cgi/cfschulung/servicediscovery/ServicediscoveryApplication.java
@@ -0,0 +1,12 @@
+package com.cgi.cfschulung.servicediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicediscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicediscoveryApplication.class, args);
+	}
+}
